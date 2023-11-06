ca036e6d47307fc766a41c3f911bc2217417251b
sheezu900 <shahzamanlive2@gmail.com>
2023-11-06 16:04:05 +0500

    setup prisma and sweggar

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,8 @@
+// src/prisma/prisma.service.ts
+
+import { Injectable } from '@nestjs/common';
+// import { INestApplication, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {}
